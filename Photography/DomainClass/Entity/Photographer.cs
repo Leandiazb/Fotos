f50a0dbf09542b5857dbbf864747fc7433fa1946
Leandiazb <80428209+Leandiazb@users.noreply.github.com>
2022-11-08 12:00:58 -0300

    Photos Page

---
+++ Photography/DomainClass/Entity/Photographer.cs
@@ -0,0 +1,40 @@
+﻿using DomainClass.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DomainClass.Entity
+{
+    public class Photographer : BaseEntityClass
+    {
+        public string? Name { get; set; }
+
+        // Hacer Validaciones de DNI- solo campos numericos y no mas de 8 caracteres 
+
+        
+        public string DNI { get; set; }
+
+        public long PhotoId { get; set; }   
+
+        public long ClientsId   { get; set; }   
+
+        public long EnventId { get; set; }
+
+        public long UserSystemId { get; set; }  
+
+        public string? LastName { get; set; }
+
+        public string? Email { get; set; }
+
+        public string? QualityPhoto { get; set; }
+
+        public string? SizePhoto { get; set; }
+
+        public long UserRol { get; set; }
+
+        
+
+    }
+}
